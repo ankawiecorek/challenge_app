5f2f1034657d601f34b9ba25d125dbfbccbded42
annawie-k <anna.wieczorek906@gmail.com>
2015-06-29 23:12:51 +0200

    devise methods added to user models

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,13 +1,6 @@
 class User < ActiveRecord::Base
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :omniauthable
-  # :recoverable, :rememberable and :trackable
-  devise :database_authenticatable, :registerable, :validatable
-
   has_many :questions
   has_many :answers
-
-  def to_s
-    email
-  end
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :trackable, :validatable
 end
