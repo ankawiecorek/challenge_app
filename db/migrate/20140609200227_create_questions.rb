9415660fec47a9f34c1c0b12dccd7ad142750a6f
annawie-k <anna.wieczorek906@gmail.com>
2015-06-29 22:12:50 +0200

    db changes - rename column 'contents'

--- db/migrate/20140609200227_create_questions.rb
+++ db/migrate/20140609200227_create_questions.rb
@@ -2,7 +2,7 @@ class CreateQuestions < ActiveRecord::Migration
   def change
     create_table :questions do |t|
       t.string :title
-      t.text :contents
+      t.string :content
 
       t.timestamps
     end
--- db/migrate/20140610104952_create_answers.rb
+++ db/migrate/20140610104952_create_answers.rb
@@ -1,13 +1,10 @@
 class CreateAnswers < ActiveRecord::Migration
   def change
     create_table :answers do |t|
-      t.integer :question_id
-      t.integer :user_id
-      t.text :contents
-
+      t.string :content
+      t.belongs_to :question
+      
       t.timestamps
     end
-    add_index :answers, :question_id
-    add_index :answers, :user_id
   end
 end
