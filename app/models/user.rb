365a2ed600426c5fe0f495e05e302202a8a52b35
annawie-k <anna.wieczorek906@gmail.com>
2015-06-30 18:06:48 +0200

    devise modified with gmail style - username instead of email displayed

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,12 +1,32 @@
 class User < ActiveRecord::Base
+  acts_as_voter
   has_many :questions
   has_many :answers
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable, :omniauthable
+         :recoverable, :rememberable, :trackable, :validatable, :omniauthable, :authentication_keys => [:login]
 
+  before_create :create_login
+
+  def login=(login)
+    @login = login
+  end
+
+  def login
+    @login || self.username || self.email
+  end
+
+  def create_login
+    email = self.email.split(/@/)
+    login_taken = User.where(:username => email[0]).first
+    unless login_taken
+      self.username = email[0]
+    else    
+      self.username = self.email
+    end        
+  end
 
   def to_s
-    email
+    username
   end
 
   def self.from_omniauth(auth)
