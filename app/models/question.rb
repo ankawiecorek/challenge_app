0a383cc767d77ee6ddbed887caa51655cfa259d4
annawie-k <anna.wieczorek906@gmail.com>
2015-06-30 22:22:03 +0200

    added association - deleting question and all of its answers

--- app/models/question.rb
+++ app/models/question.rb
@@ -1,6 +1,17 @@
 class Question < ActiveRecord::Base
-  has_many :answers
+  has_many :answers, dependent: :destroy
   belongs_to :user
   validates :title, presence: true
   validates :content, presence: true
+
+  after_update :answer_mailer
+
+  private
+
+  def answer_mailer
+    AnswerMailer.accepted_notification(self).deliver
+  end
+
+
+
 end
